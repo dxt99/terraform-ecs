e18084b46c7dbdb158e59d77c45872e25c6066a8
dxt99 <ericlouis19x@gmail.com>
2022-12-20 21:59:53 +0700

    feat: initial setup

---
+++ data.tf
@@ -0,0 +1,7 @@
+###################################################
+
+# Fetching all availability zones in ap-southeast-1
+
+###################################################
+
+data "aws_availability_zones" "azs" {}
